1d70bb2b9cbf064893a4a99869cc9e933fb0ef91
Ran Crump <rancrump18@gmail.com>
2020-02-01 22:33:33 -0600

    + Player Animations

---
+++ Assets/Scripts/Player/BodyController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyController : MonoBehaviour
+{
+    public BasicMovement movement;
+    public Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("XVelocity", transform.InverseTransformDirection(movement.rb.velocity).x);
+        anim.SetFloat("YVelocity", transform.InverseTransformDirection(movement.rb.velocity).z);
+    }
+}
