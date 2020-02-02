8c207c17279f5d33a78c06c7ae8bf1db23233f0d
Ran Crump <rancrump18@gmail.com>
2020-02-01 21:43:22 -0600

    Starting Animations

--- Assets/Scripts/BlockGeneratorScript.cs
+++ Assets/Scripts/BlockGeneratorScript.cs
@@ -33,7 +33,7 @@ public class BlockGeneratorScript : MonoBehaviour
             for (int j = 0; j < yGridSize; j++)
             {
                 GameObject block = Instantiate(blockPrefab, parentHolder.transform);
-                block.transform.position = new Vector3(i * xSpacing + (leftSide? playerLeftPos.transform.position.x: playerRightPos.transform.position.x), 1, j * ySpacing + (leftSide ? playerLeftPos.transform.position.x+6 : -playerRightPos.transform.position.x+4));
+                block.transform.position = new Vector3(i * xSpacing + (leftSide? playerLeftPos.transform.position.x: playerRightPos.transform.position.x), 1, j * ySpacing + (leftSide ? playerLeftPos.transform.position.x+7 : -playerRightPos.transform.position.x+5));
                 if (leftSide)
                     leftWalls.Add(block.GetComponent<BlockBehavior>());
                 else
